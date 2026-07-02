2e7117900107653960c7e5c163b6f849fc1ea92b
Hank Gay <hank@rescuetime.com>
2026-07-02 09:01:37 -0400

    Project skeleton created for the challenge

---
+++ Convert-o-matic/Convert-o-matic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Convert-o-matic
+//
+//  Created by Hank Gay on 7/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Convert-o-matic/Convert-o-matic/Convert_o_maticApp.swift
@@ -0,0 +1,17 @@
+//
+//  Convert_o_maticApp.swift
+//  Convert-o-matic
+//
+//  Created by Hank Gay on 7/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Convert_o_maticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
