3622f28bf41a9aacd3de90f7d088593ca6e90ea8
Hank Gay <hank@rescuetime.com>
2026-07-07 22:18:06 -0400

    Update font color to be more readable on the blue background, and randomize the countries

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,22 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-   let countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     var correctAnswer = Int.random(in: 0...2)
 
     @State private var showingAlert = false
 
     var body: some View {
-        VStack(spacing: 30) {
-            VStack {
-                Text("Tap the flag of")
-                Text(countries[correctAnswer])
-            }
-            ForEach(0..<3) { number in
-                Button {
-                    // flag was tapped
-                } label: {
-                    Image(countries[number].lowercased())
+        ZStack {
+            Color.blue.ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of").foregroundStyle(.white)
+                    Text(countries[correctAnswer]).foregroundStyle(.white)
+                }
+                ForEach(0..<3) { number in
+                    Button {
+                        // flag was tapped
+                    } label: {
+                        Image(countries[number].lowercased())
+                    }
                 }
             }
         }
