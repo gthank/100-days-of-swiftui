024438100c634c86918e828a81f765effffdc168
Hank Gay <hank@rescuetime.com>
2026-07-09 16:45:44 -0400

    And now… a View extension for convenience

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,34 +7,40 @@
 
 import SwiftUI
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct CapsuleText: View {
+    var text: String
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundStyle(.white)
+            .background(.blue)
+            .clipShape(.capsule)
+    }
+}
+
 struct ContentView: View {
-    struct Title: ViewModifier {
-        func body(content: Content) -> some View {
-            content
-                .font(.largeTitle)
-                .foregroundStyle(.white)
-                .padding()
-                .background(.blue)
-                .clipShape(.rect(cornerRadius: 10))
-        }
-    }
-
-    struct CapsuleText: View {
-        var text: String
-        var body: some View {
-            Text(text)
-                .font(.largeTitle)
-                .padding()
-                .foregroundStyle(.white)
-                .background(.blue)
-                .clipShape(.capsule)
-        }
-    }
-
     var body: some View {
         VStack(spacing: 10) {
             Spacer()
-            Text("Hello, World!").modifier(Title())
+            Text("Hello, World!").titleStyle()
             Spacer()
             CapsuleText(text: "First")
             CapsuleText(text: "Second")
