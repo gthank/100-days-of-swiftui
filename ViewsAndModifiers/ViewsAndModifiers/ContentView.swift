a8cc394148014e3a47d7bb7ffb50791cf731ea5c
Hank Gay <hank@rescuetime.com>
2026-07-09 16:43:29 -0400

    Exploring custom modifiers

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    struct Title: ViewModifier {
+        func body(content: Content) -> some View {
+            content
+                .font(.largeTitle)
+                .foregroundStyle(.white)
+                .padding()
+                .background(.blue)
+                .clipShape(.rect(cornerRadius: 10))
+        }
+    }
+
     struct CapsuleText: View {
         var text: String
         var body: some View {
@@ -22,9 +33,13 @@ struct ContentView: View {
 
     var body: some View {
         VStack(spacing: 10) {
+            Spacer()
+            Text("Hello, World!").modifier(Title())
+            Spacer()
             CapsuleText(text: "First")
             CapsuleText(text: "Second")
-        }
+            Spacer()
+        }.padding()
     }
 }
 
