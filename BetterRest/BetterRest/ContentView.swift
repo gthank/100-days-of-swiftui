621a814f718d58e1ab15551e81ee44261b521275
Hank Gay <hank@rescuetime.com>
2026-07-15 11:35:36 -0400

    Add ability to pick date

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -7,17 +7,36 @@
 
 import SwiftUI
 
+extension Calendar {
+    var tomorrowAt7AM: Date {
+        let now = Date.now
+        let targetComponents = DateComponents(hour: 7, minute: 0, second: 0)
+        // If we wind up in a place where we can't find 7AM the next day, then we
+        // probably have bigger fish to fry. I'm going unsafe unwrap.
+        return Calendar.current.nextDate(
+            after: now,
+            matching: targetComponents,
+            matchingPolicy: .nextTime,
+        )!
+    }
+}
+
 struct ContentView: View {
     @State private var sleepAmount = 8.0
 
+    @State private var wakeUpNow = Calendar.current.tomorrowAt7AM
+
     var body: some View {
         VStack {
+            Text("BetterRest").font(.largeTitle)
             Stepper(
                 "Sleep \(sleepAmount.formatted()) hours",
                 value: $sleepAmount,
                 in: 4...12,
                 step: 0.25,
             )
+            DatePicker("Please choose a date", selection: $wakeUpNow)
+            Spacer()
         }
         .padding()
     }
