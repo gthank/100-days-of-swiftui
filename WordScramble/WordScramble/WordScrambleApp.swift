09a30385e9f8aab8adc1343bc1915a0355dcbe6d
Hank Gay <hank@rescuetime.com>
2026-07-24 17:53:45 -0400

    Skeleton for project 5

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Hank Gay on 7/24/26.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
