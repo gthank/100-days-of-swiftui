4207b0e5e8da6971c9310418375604aa22bf9124
Hank Gay <hank@rescuetime.com>
2026-07-27 12:55:35 -0400

    Implement actual gameplay

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var words = [String]()
-    @State private var root = ""
+    @State private var root = "miccheck"
     @State private var newWord = ""
+
+    @State private var errTitle = ""
+    @State private var errMsg = ""
+    @State private var isErrorShowing = false
+
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL, encoding: .ascii) {
+                let allWords = startWords.components(separatedBy: "\n")
+
+                root = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+
+        fatalError("Could not load starting word list (start.txt) from bundle.")
+    }
+
+    func showError(title: String, message: String) {
+        errTitle = title
+        errMsg = message
+        isErrorShowing = true
+    }
 
     var body: some View {
         NavigationStack {
@@ -29,9 +52,47 @@ struct ContentView: View {
                     }
                 }
             }
+            .navigationTitle(root)
+            .onSubmit(addWord)
+            .onAppear(perform: startGame)
+            .alert(errTitle, isPresented: $isErrorShowing) { } message: {
+                Text(errMsg)
+            }
         }
-        .navigationTitle(root)
-        .onSubmit(addWord)
+    }
+
+    func isOriginal(word: String) -> Bool {
+        !words.contains(word)
+    }
+
+    func isPossible(word: String) -> Bool {
+        var tempWord = root
+
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    func isItAReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        // If you're as annoyed about that utf16 as me, just know that it's
+        // because UIKit is old as balls.
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(
+            in: word,
+            range: range,
+            startingAt: 0,
+            wrap: false,
+            language: "en",
+        )
+        return misspelledRange.location == NSNotFound
+
     }
 
     func addWord() {
@@ -39,6 +100,29 @@ struct ContentView: View {
             .lowercased()
         guard answer.count > 0 else { return }
 
+        guard isOriginal(word: newWord) else {
+            showError(
+                title: "I Heard You The First Time",
+                message: "You already used that one.",
+            )
+            return
+        }
+
+        guard isPossible(word: newWord) else {
+            showError(
+                title: "Inconceivable!",
+                message: "You can't make that word with those letters.",
+            )
+            return
+        }
+
+        guard isItAReal(word: newWord) else {
+            showError(
+                title: "I don't think so, Tim",
+                message: "Nice try, but that's not a real word.",
+            )
+            return
+        }
         withAnimation {
             words.insert(answer, at: 0)
         }
