5690e01410a460ff8c24dc59a73df89327b2ae01
Hank Gay <hank@rescuetime.com>
2026-07-27 08:32:16 -0400

    First step of the wordscramble game

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var words = [String]()
+    @State private var root = ""
+    @State private var newWord = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+
+                Section {
+                    ForEach(words, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
         }
-        .padding()
+        .navigationTitle(root)
+        .onSubmit(addWord)
+    }
+
+    func addWord() {
+        let answer = newWord.trimmingCharacters(in: .whitespacesAndNewlines)
+            .lowercased()
+        guard answer.count > 0 else { return }
+
+        withAnimation {
+            words.insert(answer, at: 0)
+        }
+        newWord = ""
     }
 }
 
