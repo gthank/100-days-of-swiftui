e4dbd39cd42da89e83c69c001cd336290a852638
Hank Gay <hank@rescuetime.com>
2026-07-30 14:50:50 -0400

    Explore explicit animations

--- ProjectSixPlayground/ProjectSixPlayground/ContentView.swift
+++ ProjectSixPlayground/ProjectSixPlayground/ContentView.swift
@@ -8,33 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
 
     var body: some View {
-        VStack {
-            Button("Tap Me") {
-//                animationAmount += 1
-            }
-            .padding(50)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-            .overlay(
-                Circle()
-                    .stroke(.red)
-                    .scaleEffect(animationAmount)
-                    .opacity(2 - animationAmount)
-                    .animation(
-                        .easeInOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                        value: animationAmount,
-                    )
-            )
-            .onAppear {
-                animationAmount = 2
+        Button("Tap me") {
+            withAnimation {
+                animationAmount += 360
             }
         }
-        .padding()
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .rotation3DEffect(
+            .degrees(animationAmount),
+            axis: (x: 0, y: 1, z: 0)
+        )
     }
 }
 
