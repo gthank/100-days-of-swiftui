794eb1a9e3cd79d8c354c6babfafcbd3e8b01ed4
Hank Gay <hank@rescuetime.com>
2026-07-29 20:59:15 -0400

    First round of experimenting with animations and blur effects.

---
+++ ProjectSixPlayground/ProjectSixPlayground/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  ProjectSixPlayground
+//
+//  Created by Hank Gay on 7/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 1.0
+
+    var body: some View {
+        VStack {
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
+            .blur(radius: (animationAmount - 1) * 3)
+            .animation(
+                .default,
+                value: animationAmount,
+            )
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjectSixPlayground/ProjectSixPlayground/ProjectSixPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSixPlaygroundApp.swift
+//  ProjectSixPlayground
+//
+//  Created by Hank Gay on 7/29/26.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSixPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
