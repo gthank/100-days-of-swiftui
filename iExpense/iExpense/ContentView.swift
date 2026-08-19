cee58a4c54239672743a221beb2a76e7a11dee2a
Hank Gay <hank@rescuetime.com>
2026-08-19 07:43:17 -0400

    Lists and onDelete and toolbars. Oh my!

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -9,26 +9,30 @@ import SwiftUI
 import Observation
 
 struct ContentView: View {
-    @State private var showingSecondSheet = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
 
-    var body: some View {
-        Button("Show Sheet") {
-            showingSecondSheet.toggle()
-        }.sheet(isPresented: $showingSecondSheet) {
-            SecondView(name: "Baggins. Bilbo Baggins.")
-        }
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
-}
-
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-
-    let name: String
 
     var body: some View {
-        Text("Hello, \(name)!")
-        Button("Dismiss") {
-            dismiss()
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }.onDelete(perform: removeRows)
+                }
+
+                Button("Add a number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
         }
     }
 }
