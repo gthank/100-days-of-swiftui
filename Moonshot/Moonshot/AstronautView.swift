ea20e84eb509f86390b347b3768baf845fb79b52
Hank Gay <hank@rescuetime.com>
2026-09-13 22:43:58 -0400

    Astronaut details view

---
+++ Moonshot/Moonshot/AstronautView.swift
@@ -0,0 +1,35 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Hank Gay on 9/13/26.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .foregroundStyle(.white)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    return AstronautView(astronaut: astronauts.values.randomElement()!)
+        .preferredColorScheme(.dark)
+}
