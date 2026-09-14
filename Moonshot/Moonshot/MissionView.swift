fd5a8e6a93eb1e5c2fd457bd09f5ea845f2091e0
Hank Gay <hank@rescuetime.com>
2026-09-13 21:54:03 -0400

    First stab at a Details view

---
+++ Moonshot/Moonshot/MissionView.swift
@@ -0,0 +1,45 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Hank Gay on 9/13/26.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    let mission: Mission
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(mission.badge)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { width, axis in
+                        width * 0.6
+                    }
+                    .padding(.top)
+
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    return MissionView(mission: missions[0])
+        .preferredColorScheme(.dark)
+}
