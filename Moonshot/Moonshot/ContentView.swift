378534c6e4fdd868672e68544d09eb5fdc411063
Hank Gay <hank@rescuetime.com>
2026-09-17 20:59:40 -0400

    Let the user toggle between grid and list views of the missions

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -10,13 +10,29 @@ import SwiftUI
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
+    @State var displayModeButtonLabel = "Grid"
+    @State var isGrid: Bool = true
 
     var body: some View {
         NavigationStack {
-            MissionGrid(missions: missions, astronauts: astronauts)
-                .navigationTitle("Moonshot")
-                .background(.darkBackground)
-                .preferredColorScheme(.dark)
+            Group {
+                if isGrid {
+                    MissionGrid(missions: missions, astronauts: astronauts)
+                } else {
+                    MissionList(missions: missions, astronauts: astronauts)
+                }
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(displayModeButtonLabel) {
+                        isGrid.toggle()
+                        displayModeButtonLabel = isGrid ? "Grid" : "List"
+                    }
+                }
+            }
         }
     }
 }
